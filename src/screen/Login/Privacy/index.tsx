13adde6feaaf497537a99508c0fe06a5ceb902ce
seyoung8239 <seyoung8239@gmail.com>
2021-08-17 17:04:49 +0900

    add privacy page

--- src/screen/Login/Privacy/index.tsx
+++ src/screen/Login/Privacy/index.tsx
@@ -1,15 +1,54 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import './styles';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import {styles} from './styles';
 
 export default function OpenCake() {
+  const [name, setName] = useState('');
+  const [phoneNum, setPhoneNum] = useState('');
+  const [verifCode, setVerifCode] = useState('');
+  const [isVerifing, setIsVerifing] = useState(false);
+
   return (
-    <View>
-      <Text>개인정보 수집동의</Text>
-      <Text>본인인증</Text>
-      <Text>나만의 토론을 위한 본인인증 입니다.</Text>
-      <TouchableOpacity>
-        <Text>다음</Text>
+    <View style={styles.view}>
+      <Text style={styles.text}>개인정보 수집동의</Text>
+      <View style={styles.term}></View>
+      <Text style={styles.text}>본인인증</Text>
+      <Text style={styles.subText}>나만의 토론을 위한 본인인증 입니다.</Text>
+      <TextInput
+        placeholder="이름"
+        defaultValue={name}
+        onChange={input => setName(input)}
+        style={styles.input}
+      />
+      <View style={styles.verif}>
+        <TextInput
+          placeholder="000-0000-0000"
+          defaultValue={phoneNum}
+          onChange={input => setPhoneNum(input)}
+          style={styles.inputPhone}
+        />
+        <TouchableOpacity style={isVerifing ? styles.active : styles.inactive}>
+          <Text
+            style={
+              isVerifing
+                ? styles.btnVerifTextActive
+                : styles.btnVerifTextInactive
+            }
+          >
+            인증요청
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput
+        placeholder="인증번호"
+        defaultValue={verifCode}
+        onChange={input => setVerifCode(input)}
+        style={styles.input}
+      />
+      <TouchableOpacity
+        style={isVerifing ? styles.btnNextActive : styles.btnNextInactive}
+      >
+        <Text style={styles.btnNextText}>다음</Text>
       </TouchableOpacity>
     </View>
   );
--- src/screen/Login/Privacy/styles.ts
+++ src/screen/Login/Privacy/styles.ts
@@ -1,5 +1,62 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-  
+  view: {flex: 1, marginLeft: '5%', marginRight: '%'},
+  term: {
+    height: '25%',
+    backgroundColor: '#BBBBBB',
+    marginTop: 10,
+  },
+  text: {
+    marginTop: 20,
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  input: {
+    borderBottomWidth: 1,
+    marginBottom: 25,
+    fontSize: 20,
+  },
+  inputPhone: {
+    // marginBottom: 25,
+    fontSize: 20,
+  },
+  subText: {
+    marginBottom: 20,
+  },
+  verif: {flexDirection: 'row', borderBottomWidth: 1, marginBottom: 25},
+  active: {},
+  inactive: {
+    width: 80,
+    height: 38,
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: 'red',
+    position: 'absolute',
+    right: 0,
+  },
+  btnVerifTextActive: {},
+  btnVerifTextInactive: {
+    color: 'red',
+    fontWeight: 'bold',
+  },
+
+  btnNext: {},
+  btnNextActive: {},
+  btnNextInactive: {
+    width: '100%',
+    alignItems: 'center',
+    padding: 15,
+    marginTop: 20,
+    // borderWidth: 2,
+    backgroundColor: '#FFD3D3',
+    borderRadius: 10,
+  },
+  btnNextText: {
+    color: 'white',
+    fontSize: 20,
+    shadowColor: 'black',
+  },
 });
